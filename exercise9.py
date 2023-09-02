217bd0482fe5af090fb9366b34d93c528e69098e
retrident <143639064+retrident@users.noreply.github.com>
2023-09-01 20:27:08 -0700

    Create exercise9.py

---
+++ exercise9.py
@@ -0,0 +1,6 @@
+from math import pi # pi = 3.1415...  
+radii = float(input("Enter radius of circle: ")) 
+# taking user input and converting it to float  area = pi*radii*radii 
+# # calculating area, πr2 circumference = 2*pi*radii # calculating circumference , 2πr  
+# #printing calculated values print("Area of circle is:",area) 
+# print("Circumference of circle is:",circumference)
