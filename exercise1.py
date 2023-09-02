bbe5b5102fbe789445e24ed0b2aaba0453565de6
retrident <143639064+retrident@users.noreply.github.com>
2023-09-01 19:58:55 -0700

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,1 +1,15 @@
+name = "Roman O'Keefe"
 
+address = "777 Rodog Lane"
+
+city = "Rogue"
+
+state = "California"
+
+zip_code = "1717171"
+
+print("Name: " + name)
+
+print("Address: " + address)
+
+print("City, State, ZIP: " + city + ", " + state + " " + zip_code)
