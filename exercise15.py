20bb9a6b8067e80793324c66557b03217d4ef631
retrident <143639064+retrident@users.noreply.github.com>
2023-09-13 21:30:06 -0700

    Update exercise15.py

--- exercise15.py
+++ exercise15.py
@@ -1,7 +1,5 @@
-from turtle import *
-
 def mydesign1():
-    d = Pen()
+    d = turtle.Pen()
     d.screen.bgcolor("cyan")
     d.color("black")
     d.right(-45)
@@ -16,9 +14,9 @@ def mydesign1():
     d.forward(100)
     d.right(-90)
     d.forward(100)
-    
+
 def mydesign2():
-    d = Pen()
+    d = turtle.Pen()
     d.screen.bgcolor("cyan")
     d.color("black")
     d.forward(150)
@@ -32,7 +30,7 @@ def mydesign2():
     d.forward(90)
 
 def mydesign3():
-    d = Pen()
+    d = turtle.Pen()
     d.screen.bgcolor("cyan")
     d.color("black")
     for i in range(4):
@@ -63,7 +61,7 @@ def mydesign3():
     d.setposition(-100,150)
 
 def mydesign4():
-    d = Pen()
+    d = turtle.Pen()
     d.screen.bgcolor("cyan")
     d.color("black")
     d.circle(30)
@@ -81,60 +79,4 @@ def mydesign4():
     d.penup()
     d.setposition(40,-30)
     d.pendown()
-    d.circle(30)
-    d.penup()
-    d.setposition(120,-30)
-    d.pendown()
-    d.circle(30)
-
-def mydesign5():
-    d = Pen()
-    d.screen.bgcolor("cyan")
-    d.color("black")
-    d.setposition(0,0)
-    d.write("North")    
-    d.right(90)
-    d.forward(100)
-    d.penup()
-    d.forward(15)
-    d.write("South")
-    d.setposition(-50,-50)
-    d.write("West")
-    d.left(90)
-    d.pendown()
-    d.forward(100)
-    d.write("East")
-    d.penup()
-    d.setposition(0,-60)
-    d.pendown()
-    d.circle(10)
-    
-def mydesign6():
-    d = Pen()
-    d.screen.bgcolor("cyan")
-    d.color("black")
-    d.dot(5,"black")
-    d.right(90)
-    d.forward(150)
-    d.dot(5,"black")
-    d.left(135)
-    d.forward(210)
-    d.dot(5,"black")
-    d.right(135)
-    d.dot(5,"black")
-    d.forward(150)
-    d.dot(5,"black")
-    d.setposition(0,0)
-    d.left(90)
-    for i in range(15):
-        d.forward(5)
-        d.penup()
-        d.forward(5)
-        d.pendown()
-    d.penup()
-    d.setposition(0,-150)
-    for i in range(15):
-        d.forward(5)
-        d.penup()
-        d.forward(5)
-        d.pendown()
+    d.circle(3)
